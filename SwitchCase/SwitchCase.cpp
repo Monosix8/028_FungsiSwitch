391dd899f5efe759896cd938e54ee14ec301d8c6
Monosix8 <162055149+Monosix8@users.noreply.github.com>
2024-03-26 17:52:21 +0700

    Add project files.

---
+++ SwitchCase/SwitchCase.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+	cout << "Masukan Bilangan Pertama = ";
+	cin >> bilA;
+	cout << "Masukan Bilangan Kedua = ";
+	cin >> bilB;
+}
+
+int penjumlahan(int a, int b)
+{
+	return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+	return a + b;
+}
+
+int perkalian(int a, int b)
+{
+	return a + b;
+}
+
+float pembagian(float a, float b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int pilihan;
+	do
+	{
+		cout << "Kalkulator Sederhana" << endl;
+		cout << "====================" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "====================" << endl;
+		cout << "Masukan menu pilihan : ";
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
